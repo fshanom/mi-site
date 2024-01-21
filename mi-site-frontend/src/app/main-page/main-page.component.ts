a54576b45b3771e9a4b3f9476208273f387567f6
fshanom <filipeshanom@gmail.com>
2024-01-20 23:29:21 -0300

    Main page and adding social media information

--- mi-site-frontend/src/app/main-page/main-page.component.ts
+++ mi-site-frontend/src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
